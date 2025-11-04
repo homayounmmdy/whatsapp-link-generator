c33ccc872ca147548f26067fa8f2d051456de456
Homayoun <homayoun763@gmail.com>
2025-11-05 00:13:27 +0330

    fix select arrow for rtl

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -1,3 +1,4 @@
+import { useDirection } from "@/app/hooks/useDirection";
 import { useTranslations } from "next-intl";
 
 interface Country {
@@ -11,11 +12,18 @@ interface CountrySelectProps {
   onChange: (value: string) => void;
 }
 
-export default function CountrySelect({ countries, value, onChange }: CountrySelectProps) {
-  const t = useTranslations('whatsappLinkGenerator');
+export default function CountrySelect({
+  countries,
+  value,
+  onChange,
+}: CountrySelectProps) {
+  const t = useTranslations("whatsappLinkGenerator");
+  const { rtl } = useDirection();
   return (
     <div>
-      <label className="mb-1 block text-sm font-medium text-gray-700">{t('country')}</label>
+      <label className="mb-1 block text-sm font-medium text-gray-700">
+        {t("country")}
+      </label>
       <div className="relative">
         <select
           value={value}
@@ -28,9 +36,25 @@ export default function CountrySelect({ countries, value, onChange }: CountrySel
             </option>
           ))}
         </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-gray-500">
-          <svg className="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+        <div
+          className={`${rtl(
+            "left-0",
+            "right-0"
+          )} pointer-events-none absolute inset-y-0  flex items-center px-3 text-gray-500`}
+        >
+          <svg
+            className="h-4 w-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
           </svg>
         </div>
       </div>
